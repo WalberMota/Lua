9fb3d51505bc33a8acd4e403b45ba296a81f5c22
Walber S Mota <mota.csharp@gmail.com>
2023-10-04 23:04:17 -0300

    update test.lua

--- test.lua
+++ test.lua
@@ -7,6 +7,8 @@ local nome
 print("digite o seu nome\n")
 nome=io.read()
 
-print("\x1b[2J\x1b[1;1H") --limpa a tela
+print("\x1b[2J\x1b[1;1H") --limpa a tela e coloca o cursos na linha 1 e coluna 1
+--print("\x1b[2J")  --limpa somente a tela
+
 io.write("Olá ", nome, " seja bem vindo à Lua kkkk")
 
